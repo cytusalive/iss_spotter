8bf99f078bd0147b7d16938e34b97216fdf4121f
cytusalive <cytusomega@gmail.com>
2021-07-02 06:44:59 +0000

    added fetch coordinates

--- iss.js
+++ iss.js
@@ -25,4 +25,22 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    callback(null, {'latitude': data.latitude, 'longitude': data.longitude});
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
